19880a47ed48051b519a056e514c1ecfd7a1af3a
Tim White <timwhite47@gmail.com>
2015-10-08 11:49:57 -0700

    add header and footer to sundew.

--- assets/javascripts/components/as-labs-footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export class Footer extends React.Component {
-  render() {
-    return <b>{this.props.message}</b>
-  }
-}
--- assets/javascripts/sundew.jsx
+++ assets/javascripts/sundew.jsx
@@ -1,7 +1,40 @@
 window.AsLabs = {
   Footer: React.createClass({
     render: function () {
-      return <b>{this.props.message}</b>
+      return <footer id="footer">
+        <section className="container">
+          <div className="left">
+            <a href="http://actionsprout.com/labs" className="logo">
+              <img src={this.props.lab_logo_src} alt="actionsprout labs"/>
+            </a>
+          </div>
+          <div className="right">
+            <a href={this.props.sign_up_href} className="ui button tiny">Sign Up</a>
+            <p className="more">Try ActionSprout for Free</p>
+          </div>
+        </section>
+      </footer>
+    }
+  }),
+  Header: React.createClass({
+    render: function () {
+      return <section id="header">
+        <section className="container">
+          <div className="ui floated left">
+            <a href="/">
+              <img src={this.props.logo_src} />
+            </a>
+          </div>
+          <div className="ui floated right">
+            <p>Powered by</p>
+
+            <a href={this.props.logo_link}>
+              <img src={this.props.horizontal_logo_src} className='logo'/>
+            </a>
+          </div>
+        </section>
+      </section>
+
     }
   })
 };
