4801be5ddaf2d02659a0b1958f0a83e2551f8826
Tim White <timwhite47@gmail.com>
2015-11-06 13:02:43 -0800

    build.

--- assets/javascripts/dist/index.js
+++ assets/javascripts/dist/index.js
@@ -3,5 +3,6 @@
 window.AsLabs = require('react-semantify/lib/index_browser.js');
 require('./header');
 require('./footer');
+require('./avatar');
 require('./share_button');
 require('./login_button');
